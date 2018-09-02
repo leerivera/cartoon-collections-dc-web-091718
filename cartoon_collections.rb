a883492f56da4d6df98afa112c1f200528041e00
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-02 03:49:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(array)# code an argument here
     end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+  array.collect do |word| 
+   word.capitalize + "!"
+ end 
 end
 
 def long_planeteer_calls# code an argument here
